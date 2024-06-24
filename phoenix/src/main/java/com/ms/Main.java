06a4a4a3b345d3a21994030f65d91fa11cdae2e8
Maneesh Singh <maneesh54321@gmail.com>
2024-06-24 23:53:38 +0530

    [Phoenix] progress

--- phoenix/src/main/java/com/ms/Main.java
+++ phoenix/src/main/java/com/ms/Main.java
@@ -1,11 +1,13 @@
 package com.ms;
 
 import com.ms.backup.BackupManager;
-import com.ms.backup.BackupMetadataLoader;
+import com.ms.backup.DiffFinder;
 
 public class Main {
     public static void main(String[] args) {
-        BackupManager backupManager = new BackupManager();
+        var diffFinder = new DiffFinder();
+        diffFinder.init();
+        BackupManager backupManager = new BackupManager(diffFinder);
 
         backupManager.startBackup();
     }
